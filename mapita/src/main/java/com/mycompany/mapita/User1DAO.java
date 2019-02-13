5d6ce3f108e82120487d78cff5f150b4a35e0a33
afloresl <cani@ciencias.unam.mx>
2019-02-12 20:08:07 -0600

    Ejercicio03

---
+++ mapita/src/main/java/com/mycompany/mapita/User1DAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mapita;
+
+/**
+ *
+ * @author ana
+ */
+public class User1DAO extends AbstractDAO<User1>{
+    
+}
